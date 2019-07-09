014cf164ca87a8b8541606bfb7fd1860f802c05b
silvandeleemput <silvandeleemput@gmail.com>
2019-07-09 15:21:53 +0200

    Added rtd_theme intersphinx extension and numpy mock module #17

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import os
 import sys
 import mock
 
-MOCK_MODULES = ['torch', 'torch.nn']
+MOCK_MODULES = ['torch', 'torch.nn', 'numpy', 'tensorflow']
 for mod_name in MOCK_MODULES:
    sys.modules[mod_name] = mock.Mock()
 
@@ -39,7 +39,8 @@ import memcnn.models.revop
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax',
+              'sphinx.ext.napoleon', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -104,7 +105,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
