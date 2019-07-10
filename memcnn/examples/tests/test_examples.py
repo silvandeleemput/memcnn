ef9ba69b210f79ff72c1317a9b239d9a792d4070
silvandeleemput <silvandeleemput@gmail.com>
2019-07-10 13:17:37 +0200

    attempt fix test_examples.py through subprocess instead of os.system

--- memcnn/examples/tests/test_examples.py
+++ memcnn/examples/tests/test_examples.py
@@ -1,9 +1,11 @@
 import os
+import subprocess as sp
 import sys
 import memcnn
 
 
 def test_example_minimal():
-    example_fname = os.path.join(os.path.dirname(memcnn.__file__), 'examples', 'minimal.py')
-    ret_code = os.system(sys.executable + ' ' + example_fname)
-    assert(ret_code == 0)
+    example_fname = os.path.join(os.path.abspath(os.path.dirname(memcnn.__file__)), 'examples', 'minimal.py')
+    p = sp.Popen([os.path.abspath(sys.executable), example_fname], stdout=sp.DEVNULL, stdin=sp.DEVNULL)
+    p.communicate()
+    assert(p.returncode == 0)
