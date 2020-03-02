fd3bdb9176984d08de90d666eb1428470b77c422
silvandeleemput <silvandeleemput@gmail.com>
2020-03-02 09:32:47 +0100

    Added tracemalloc for CPU memory measurements #18

--- memcnn/models/tests/test_memory_saving.py
+++ memcnn/models/tests/test_memory_saving.py
@@ -4,11 +4,20 @@ import numpy as np
 import torch
 import torch.nn
 from memcnn.models.tests.test_revop import SubModuleStack, SubModule
+import tracemalloc
 
 
-@pytest.mark.parametrize('coupling', ['additive', 'affine'])
-@pytest.mark.parametrize('keep_input', [True, False])
-@pytest.mark.parametrize('device', ['cpu', 'cuda'])
+def get_allocated_memory(device):
+    return (
+        torch.cuda.memory_allocated() / float(1024 ** 2)
+        if device == "cuda"
+        else tracemalloc.get_traced_memory()[0] / float(1024 ** 2)
+    )
+
+
+@pytest.mark.parametrize("coupling", ["additive", "affine"])
+@pytest.mark.parametrize("keep_input", [True, False])
+@pytest.mark.parametrize("device", ["cpu", "cuda"])
 def test_memory_saving_invertible_model_wrapper(device, coupling, keep_input):
     """Test memory saving of the invertible model wrapper
 
@@ -21,12 +30,8 @@ def test_memory_saving_invertible_model_wrapper(device, coupling, keep_input):
       for a depth=5 this yields                                  7.8125 * 5 = 39.0625 kB
 
     """
-
-    if device == 'cpu':
-        pytest.skip('Unreliable metrics, should be fixed.')
-
-    if device == 'cuda' and not torch.cuda.is_available():
-        pytest.skip('This test requires a GPU to be available')
+    if device == "cuda" and not torch.cuda.is_available():
+        pytest.skip("This test requires a GPU to be available")
 
     gc.disable()
     gc.collect()
@@ -39,32 +44,39 @@ def test_memory_saving_invertible_model_wrapper(device, coupling, keep_input):
         ytarget = torch.rand(*dims, device=device, dtype=torch.float32)
 
         # same convolution test
-        network = SubModuleStack(SubModule(in_filters=5, out_filters=5), depth=depth, keep_input=keep_input, coupling=coupling,
-                                 implementation_fwd=-1, implementation_bwd=-1)
+        network = SubModuleStack(
+            SubModule(in_filters=5, out_filters=5),
+            depth=depth,
+            keep_input=keep_input,
+            coupling=coupling,
+            implementation_fwd=-1,
+            implementation_bwd=-1,
+        )
         network.to(device)
         network.train()
         network.zero_grad()
         optim = torch.optim.RMSprop(network.parameters())
         optim.zero_grad()
-        mem_start = 0 if not device == 'cuda' else \
-            torch.cuda.memory_allocated() / float(1024 ** 2)
+        mem_start = get_allocated_memory(device=device)
+        tracemalloc.start()
 
         y = network(xx)
         gc.collect()
-        mem_after_forward = torch.cuda.memory_allocated() / float(1024 ** 2)
+        mem_after_forward = get_allocated_memory(device=device)
         loss = torch.nn.MSELoss()(y, ytarget)
         optim.zero_grad()
         loss.backward()
         optim.step()
         gc.collect()
-        # mem_after_backward = torch.cuda.memory_allocated() / float(1024 ** 2)
         gc.enable()
+        tracemalloc.stop()
 
-        memuse = float(np.prod(dims + [depth, 4, ])) / float(1024 ** 2)
+        memuse = (
+            float(np.prod(dims + [depth, 4])) / float(1024 ** 2)
+            if device == "cuda"
+            else 0.0056
+        )
 
         measured_memuse = mem_after_forward - mem_start
-        if keep_input:
-            assert measured_memuse >= memuse
-        else:
-            assert measured_memuse < 1
-        # assert math.floor(mem_after_backward - mem_start) >= 9
+
+        assert (measured_memuse >= memuse) == keep_input
